6f388747399bbce8fa597cac2674735f7c64918b
Sofie Urban <sofiehu@student.uia.no>
2021-01-25 19:41:44 +0100

    added buttons

--- app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -14,19 +14,68 @@ class MainActivity : AppCompatActivity() {
     lateinit var timer:CountDownTimer
     lateinit var startButton:Button
     lateinit var coutdownDisplay:TextView
+    lateinit var thirtym:Button
+    lateinit var sixtym:Button
+    lateinit var ninetym:Button
+    lateinit var hundredTwentym:Button
 
-    val timeToCountDownInMs = 5000L
+
+    var timeToCountDownInMs = 6000L
     val timeTicks = 1000L
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       startButton = findViewById<Button>(R.id.startCountdownButton)
-       startButton.setOnClickListener(){
-           startCountDown(it)
-       }
-       coutdownDisplay = findViewById<TextView>(R.id.countDownView)
+        startButton = findViewById<Button>(R.id.startCountdownButton)
+        startButton.setOnClickListener(){
+            startCountDown(it)
+            startButton.isClickable=false
+            thirtym.isClickable=false
+            sixtym.isClickable=false
+            ninetym.isClickable=false
+            hundredTwentym.isClickable=false
+
+        }
+        //30 min knapp
+        thirtym = findViewById(R.id.button30Minutes)
+        thirtym.setOnClickListener(){
+
+            timeToCountDownInMs = 1800000L
+            updateCountDownDisplay(timeToCountDownInMs)
+
+
+            //60 min knapp
+        }
+        sixtym = findViewById(R.id.button60Minutes)
+        sixtym.setOnClickListener(){
+
+            timeToCountDownInMs = 3600000L
+            updateCountDownDisplay(timeToCountDownInMs)
+
+
+        }
+        //90 min knapp
+        ninetym = findViewById(R.id.button90Minutes)
+        ninetym.setOnClickListener(){
+
+            timeToCountDownInMs = 5400000L
+            updateCountDownDisplay(timeToCountDownInMs)
+
+
+        }
+        //120 min knapp
+        hundredTwentym = findViewById(R.id.button120Minutes)
+        hundredTwentym.setOnClickListener(){
+
+            timeToCountDownInMs = 7200000L
+            updateCountDownDisplay(timeToCountDownInMs)
+
+
+        }
+
+        coutdownDisplay = findViewById<TextView>(R.id.countDownView)
 
     }
 
@@ -35,10 +84,15 @@ class MainActivity : AppCompatActivity() {
         timer = object : CountDownTimer(timeToCountDownInMs,timeTicks) {
             override fun onFinish() {
                 Toast.makeText(this@MainActivity,"Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
+                thirtym.isClickable=true
+                sixtym.isClickable=true
+                ninetym.isClickable=true
+                startButton.isClickable=true
+                hundredTwentym.isClickable=true
             }
 
             override fun onTick(millisUntilFinished: Long) {
-               updateCountDownDisplay(millisUntilFinished)
+                updateCountDownDisplay(millisUntilFinished)
             }
         }
 
@@ -48,5 +102,6 @@ class MainActivity : AppCompatActivity() {
     fun updateCountDownDisplay(timeInMs:Long){
         coutdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
     }
+
 
 }
